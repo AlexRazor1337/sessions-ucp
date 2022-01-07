7b660bf6baa2060c1c9d51a6dd64acad6f8c64f5
AlexRazor1337 <alexrazor1337@gmail.com>
2022-01-07 23:06:21 +0200

    Add secret env

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 require('dotenv').config()
 
 const bcrypt = require('bcrypt')
+
 const AdminJS = require('adminjs')
 const AdminJSExpress = require('@adminjs/express')
 const AdminJSSequelize = require('@adminjs/sequelize')
 AdminJS.registerAdapter(AdminJSSequelize)
-const db = require('./models')
+
 const express = require('express')
 const app = express()
+
+const adminJs = require('./routes/admin')
+const db = require('./models')
 
 const compression = require('compression')
 app.use(compression())
 
 const port = process.env.PORT || 3000
 
-const adminJs = require('./routes/admin')
-// const router = AdminJSExpress.buildRouter(adminJs)
 const router = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
     authenticate: async (email, password) => {
         const allowedRoles = ['admin', 'superadmin']
@@ -28,7 +30,7 @@ const router = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
         }
         return false
     },
-    cookiePassword: 'some-secret-password-used-to-secure-cookie',
+    cookiePassword: process.env.SECRET,
 })
 app.use(adminJs.options.rootPath, router)
 
