2b63a52016e6db5176641d6316cdfb45017fd57f
AlexRazor1337 <alexrazor1337@gmail.com>
2022-02-02 22:46:28 +0200

    Add body parsing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+app.use(express.json())
 app.use(require('compression')())
 
 const port = process.env.PORT || 3000
