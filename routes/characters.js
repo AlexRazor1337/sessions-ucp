ac0488519d956960c714fa5c32e6aa6fa462acfd
AlexRazor1337 <alexrazor1337@gmail.com>
2022-02-05 22:29:41 +0200

    Basic character creation route

--- routes/characters.js
+++ routes/characters.js
@@ -4,10 +4,23 @@ const Character = require('../models').character;
 const authMiddleware = require('../middleware/auth');
 
 charactersRouter.get('/', authMiddleware, async (req, res) => {
-    const id = req.account.id;
-    const characters = await Character.findAll({ where: { accountId: id } });
+    const accountId = req.account.id;
+    const characters = await Character.findAll({ where: { accountId } });
 
     res.send(characters);
+});
+
+
+charactersRouter.post('/', authMiddleware, async (req, res) => {
+    const accountId = req.account.id;
+    const charactersCount = await Character.count({ where: { accountId } });
+    if (charactersCount === 4) {
+        return res.status(400).send('You can\t create more characters!');
+    }
+    // name, skin, description, sex, accountId
+    const {name, skin, description, sex} = req.body;
+    const character = await Character.create({name, skin, description, sex, accountId});
+    res.send(character);
 });
 
 
