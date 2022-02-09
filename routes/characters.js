0a0e462972e33837e0f7f7a5ed20f8a9dff0f262
AlexRazor1337 <alexrazor1337@gmail.com>
2022-02-09 19:57:40 +0200

    use fastest validator as character validator

--- routes/characters.js
+++ routes/characters.js
@@ -2,6 +2,15 @@ const charactersRouter = require('express').Router();
 const Account = require('../models').account;
 const Character = require('../models').character;
 const authMiddleware = require('../middleware/auth');
+const Validator = new (require('fastest-validator'));
+
+const characterValidationSchema = {
+    name: { type: 'string', min: 4, max: 32 },
+    skin: { type: 'number', min: 1, max: 350, integer: true, optional: true },
+    description: { type: 'string', min: 1, max: 256, optional: true },
+    sex: { type: 'enum', values: ['male', 'female'] }
+};
+const check = Validator.compile(characterValidationSchema);
 
 charactersRouter.get('/', authMiddleware, async (req, res) => {
     const accountId = req.account.id;
@@ -12,12 +21,17 @@ charactersRouter.get('/', authMiddleware, async (req, res) => {
 
 
 charactersRouter.post('/', authMiddleware, async (req, res) => {
+    const validationResult = check(req.body);
+    if (validationResult !== true) {
+        return res.status(422).send(validationResult.map(error => error.message));
+    }
+
     const accountId = req.account.id;
     const charactersCount = await Character.count({ where: { accountId } });
     if (charactersCount === 4) {
         return res.status(400).send('You can\t create more characters!');
     }
-    // name, skin, description, sex, accountId
+
     const {name, skin, description, sex} = req.body;
     const character = await Character.create({name, skin, description, sex, accountId});
     res.send(character);
